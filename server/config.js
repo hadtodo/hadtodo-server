69206a84dfc2e27b890252ce65c9dceb90eedede
m4tx <m4tx@m4tx.pl>
2015-02-27 18:09:02 +0100

    Add license info to all js files, minor comment fixes

--- server/config.js
+++ server/config.js
@@ -1,4 +1,19 @@
-/**
+/*
+ * hadtodo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * hadtodo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with hadtodo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  * Config stuff
  */
 
@@ -16,7 +31,7 @@ var config = {};
  */
 config.server = {};
 /**
- * A address where the server should run,
+ * An address where the server should run,
  * by default uses environment variable 'HOST'
  * or if not set: '0.0.0.0'
  * @type {string} [host='0.0.0.0']
